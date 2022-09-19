55fc0a1a51b9ae9a3a3dbd76e281bf5886dc17f4
Jun <kumejun2000@gmail.com>
2022-09-20 06:32:19 +0900

    色々アップデート

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import os
-import join
-import plot
+import sys
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def main():
-    join.create_csv()
-    plot.plot()
-    os.remove('total.csv')
+def main(args):
+    csv_files = glob.glob('./csv/' + args[2] + '/*.csv')
+    for file in csv_files:
+        name = os.path.splitext(os.path.basename(file))[0]
+        data = pd.read_csv(file)
+        plt.plot(data, label=name)
+    plt.legend()
+    plt.title(args[1])
+    plt.xlabel('step')
+    plt.ylabel('regret')
+    plt.savefig('./csv/' + args[2] + '/csv_plot.png')
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    if len(args) <= 1:
+        print('wrong number of arguments')
+        sys.exit()
+    main(args)
--- plot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def plot():
-    data = pd.read_csv('total.csv')
-    plt.plot(data, label=data.columns)
-    plt.legend()
-    plt.savefig('multi_arm.png')
-    plt.show()
